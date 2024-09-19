85d5513f5648e4ec727d13fac4359b312d99bc7b
GiorgioDordo <djole6789@gmail.com>
2024-09-19 06:41:39 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+
+export default {
+  data () {
+    return {
+    }
+  },
+  components: [
+    AppHeader,
+    AppMain
+  ]
+}
+</script>
+
+<template>
+<AppHeader />
+<AppMain />
+</template>
+
+<style lang="scss" scoped>
+@use '../node_modules/bootstrap/scss/bootstrap.scss'
+@use './general.scss'
+</style>
