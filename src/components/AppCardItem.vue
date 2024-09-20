e9354f2b16a3f4040b6fdbf40014e8cb6ce7ca34
GiorgioDordo <djole6789@gmail.com>
2024-09-20 04:15:22 +0200

    added infos below the cards

--- src/components/AppCardItem.vue
+++ src/components/AppCardItem.vue
@@ -18,7 +18,10 @@ export default {
     <article class="card w-100">
         <img :src=" cardObject.card_images[0].image_url " class="card-img-top" alt="...">
         <div class="card-body">
+            <p class="card-text"><strong>Archetype:</strong>{{ cardObject.archetype }}</p>
             <p class="card-text"><strong>Name:</strong>{{ cardObject.name }}</p>
+            <p class="card-text"><strong>Race:</strong>{{ cardObject.race }}</p>
+            <p class="card-text"><strong>Type:</strong>{{ cardObject.type }}</p>
         </div>
     </article>
 </div>
